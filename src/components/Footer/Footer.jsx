c568bc0661c9fe76124348b63848cc5065cf4448
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-04 20:04:27 +0530

    upto footer ul and li

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,33 @@
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
     return (
         <div className='footer'>
-            Footer
+            <div className="footer-icon">
+                <img src={facebook_icon} alt="facebook" />
+                <img src={instagram_icon} alt="instagram" />
+                <img src={twitter_icon} alt="twitter" />
+                <img src={youtube_icon} alt="youtube" />
+            </div>
+
+            <ul>
+                <li>Audio Description</li>
+                <li>Help Center</li>
+                <li>Gift Center</li>
+                <li>Media Center</li>
+                <li>Inventor Relation</li>
+                <li>Jobs</li>
+                <li>Terms of Use</li>
+                <li>Privacy</li>
+                <li>Legal Notice</li>
+                <li>Cookie Preference</li>
+                <li>Corporation Information</li>
+                <li>Contact Us</li>
+            </ul>
         </div>
     )
 }
