e21e750ddb332c6c09574ef3e63a52fb64b92866
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-05 21:44:19 +0530

    title and home page responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
                         <button className='btn dark-btn'><img src={info_icon} alt="info-icon" />More Info</button>
                     </div>
 
-                    <TitleCards />
+                    <TitleCards className="title-cards"/>
 
                 </div>
             </div>
