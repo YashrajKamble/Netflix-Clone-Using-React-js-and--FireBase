479c7d32b30653465a55038e919741954364f2b8
Yashraj_K <yashrajkamble7727@gamil.com>
2024-08-05 18:13:31 +0530

    Firebase initialization is done

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwxLRTvZTqokIRZn6Nwt8_BY3o0TDLRmw",
+    authDomain: "netflix-clone-9b146.firebaseapp.com",
+    projectId: "netflix-clone-9b146",
+    storageBucket: "netflix-clone-9b146.appspot.com",
+    messagingSenderId: "1061429835549",
+    appId: "1:1061429835549:web:35d5b2c47ba18fd64453f4"
+};
+
+const app = initializeApp(firebaseConfig);
